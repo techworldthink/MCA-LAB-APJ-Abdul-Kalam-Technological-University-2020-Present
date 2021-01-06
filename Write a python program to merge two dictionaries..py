89e3184dbb1c1b4fdac4f32fd79b144633c49dd5
Jobin j <60466460+techworldthink@users.noreply.github.com>
2021-01-06 11:21:28 +0530

    Update Write a python program to merge two dictionaries..py
    dictionary merge

--- Write a python program to merge two dictionaries..py
+++ Write a python program to merge two dictionaries..py
@@ -1,13 +1,29 @@
 dict1={}
 dict2={}
 dict3={}
+
 n1=int(input("Total number of elements in dict 1 : "))
 for i in range(n1):
-    dict1[i]=input("Enter element : ")
+    dict1[i]=int(input("Enter element : "))
+    
 n2=int(input("Total number of elements in dict 2 : "))
-for i in range(i+1,i+n2+1):
-    dict2[i]=input("Enter element : ")
+for i in range(3,3+n2):
+    dict2[i]=int(input("Enter element : "))
+    
 print(dict1)
 print(dict2)
-dict3 ={**dict1,**dict2}
+
+for i in dict1:
+    if i not in dict2:
+        new = {i:dict1[i]}
+        dict3.update(new)
+    else:
+        new = {i:dict1[i]+dict2[i]}
+        dict3.update(new)
+        
+for i in dict2:
+    if i not in dict1:
+        new = {i:dict2[i]}
+        dict3.update(new)
+        
 print(dict3)
