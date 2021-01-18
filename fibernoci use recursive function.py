9f9d540fe682041ff8ff7892049ae7392675009f
Jobin j <60466460+techworldthink@users.noreply.github.com>
2021-01-18 19:18:43 +0530

    Update fibernoci use recursive function.py

--- fibernoci use recursive function.py
+++ fibernoci use recursive function.py
@@ -1,4 +1,23 @@
-#This program print Fibonacci sequence #upto a given number
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+n=int(input("Enter the number:"))
+recur_fibo(n)
+for i in range(n):
+    print(recur_fibo(i))
+
+
+
+
+
+
+
+
+"""#This program print Fibonacci sequence #upto a given number
 a, b = 1, 1
 c=0
 def fibernoci(n):
@@ -13,4 +32,4 @@ def fibernoci(n):
         fibernoci(n)
     
 n=int(input("Enter the number:"))
-fibernoci(n)
+fibernoci(n)"""
