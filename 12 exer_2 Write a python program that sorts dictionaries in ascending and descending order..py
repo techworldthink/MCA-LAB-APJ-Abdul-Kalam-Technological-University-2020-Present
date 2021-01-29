5e8a7a4e941b95aeaffc0cff1392eac9fe3173ae
techworldthink <jobin25051999@gmail.com>
2021-01-29 15:17:35 +0530

    dict 12 updated

--- 12 exer_2 Write a python program that sorts dictionaries in ascending and descending order..py
+++ 12 exer_2 Write a python program that sorts dictionaries in ascending and descending order..py
@@ -2,8 +2,8 @@ dic={}
 n1=int(input("Total number of elements in dict 1 : "))
 for i in range(n1):
     dic[i]=input("Enter element : ")
-#print(dic.items())
 print(sorted(dic.items(), key = lambda kv:(kv[1], kv[0])))
+print(sorted(dic.items(), key = lambda kv:(kv[1], kv[0]), reverse=True))
 
 
 
@@ -11,11 +11,7 @@ print(sorted(dic.items(), key = lambda kv:(kv[1], kv[0])))
 """asc=sorted(dic.values())
 print(asc)
 asc.reverse()
-print(asc)"""
-
-
-
-
-#print(sorted(dic.values()))
-#print(type(sorted(dic.values())))
-#print(type(dic.values()).re)
+print(asc)
+print(sorted(dic.values()))
+print(type(sorted(dic.values())))
+print(type(dic.values()).re)"""
