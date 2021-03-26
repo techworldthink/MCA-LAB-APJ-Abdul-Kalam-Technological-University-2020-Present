c65da75537bbd0061d20785bee78f1dcb28f3efc
techworldthink <jobin25051999@gmail.com>
2021-03-26 19:11:24 +0530

    prims my programme

--- prims.c
+++ prims.c
@@ -1,39 +1,52 @@
 #include<stdio.h>
 
-int a,b,u,v,n,i,j,ne=1;
-int visited[10]={0},min,mincost=0,cost[10][10];
-  
-void main(){
+#define MAX 10
+
+int main(){
+	int vertex_count=0;
+	int row,column;
+	int cost_matrix[MAX][MAX];
+	int visited[MAX]={0};
+	int edge_count=0,count=1;
+	int sum_cost=0,min_cost=0;
+	int row_no,column_no;
 	
-   printf("\nEnter the number of nodes:");  
-   scanf("%d",&n);  printf("\nEnter the adjacency matrix:\n"); 
-   
-   for(i=1;i<=n;i++) 
-	   for(j=1;j<=n;j++){ 
-	     scanf("%d",&cost[i][j]); 
-         if(cost[i][j]==0) 
-         	cost[i][j]=999; 
-       } 
-       
-visited[1]=1; 
-printf("\n"); 
-
-while(ne < n){ 
-	for(i=1,min=999;i<=n;i++) 
-	   for(j=1;j<=n;j++) 
-		    if(cost[i][j]< min)  
-			  if(visited[i]!=0){    
-			  	min=cost[i][j]; 
-				    a=u=i;  
-					b=v=j;  
-			  }  
-			  
-	if(visited[u]==0 || visited[v]==0){  
-		printf("\n Edge %d:(%d %d) cost:%d",ne++,a,b,min); 
-		mincost+=min;    visited[b]=1;   
-	}  
-	cost[a][b]=cost[b][a]=999;  
-} 
-
-printf("\n Minimum cost=%d",mincost); 
-} 
+	printf("Total no of vertex :: ");
+	scanf("%d",&vertex_count);
+	
+	//Get edge weight matrix from user	
+	for(row=1;row<=vertex_count;row++){
+		for(column=1;column<=vertex_count;column++){
+			scanf("%d",&cost_matrix[row][column]);
+			if(cost_matrix[row][column] == 0){
+				cost_matrix[row][column] = 999;
+			}
+		}
+	}
+	
+	visited[1]=1;
+	edge_count = vertex_count-1;
+	
+	while(count <= edge_count){
+		for(row=1,min_cost=999;row<=vertex_count;row++){
+			for(column=1;column<=vertex_count;column++){
+				if(cost_matrix[row][column] < min_cost){
+					if(visited[row] != 0){
+						min_cost = cost_matrix[row][column];
+						row_no = row;
+						column_no = column;
+					}
+				}
+			}
+		}
+		
+		if(visited[row_no] == 0 || visited[column_no] ==0){
+			printf("\nEdge %d is (%d -> %d) with cost : %d ",count++,row_no,column_no,min_cost);
+			sum_cost = sum_cost + min_cost;
+			visited[column_no]=1;
+		}
+		cost_matrix[row][column] = cost_matrix[column][row] = 999;
+	}
+	printf("\n Minimum cost=%d",sum_cost); 
+	return 0;
+}
