be31f63d9522c37c6dc0a95b5dec37eb4c1cac03
techworldthink <jobin25051999@gmail.com>
2021-03-26 21:28:22 +0530

    kruskal my programme

--- kruskal.c
+++ kruskal.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #define MAX 10
 
-int main(){
+int parent[MAX];
+
+int find(int i){ 
+	while(parent[i])
+	 	i=parent[i]; 
+	return i; 
+} 
+						  
+int uni(int i,int j){
+	if(i!=j){  
+		parent[j]=i; 
+		return 1; 
+	} 
+	return 0; 
+} 
+	 
+	 
+int main(){  
 	int vertex_count=0;
 	int row,column;
 	int cost_matrix[MAX][MAX];
-	int visited[MAX]={0};
 	int edge_count=0,count=1;
-	int sum_cost=0,min_cost=0;
-	int row_no,column_no,vertex1,vertex2;
+	int sum_cost=0,min_cost;
+	int row_no,column_no,edge1,edge2;
 	
+	printf("Implementation of Kruskal's algorithm\n\n");
 	printf("Total no of vertex :: ");
 	scanf("%d",&vertex_count);
 	
@@ -24,29 +42,31 @@ int main(){
 		}
 	}
 	
-	visited[1]=1;
 	edge_count = vertex_count-1;
 	
 	while(count <= edge_count){
 		for(row=1,min_cost=999;row<=vertex_count;row++){
 			for(column=1;column<=vertex_count;column++){
 				if(cost_matrix[row][column] < min_cost){
-					if(visited[row] != 0){
 						min_cost = cost_matrix[row][column];
-						vertex1 = row_no = row;
-						vertex2 = column_no = column;
-					}
+						edge1 = row_no    =  row;
+						edge2 = column_no =  column;
 				}
 			}
-		}
+		}   
+ 
+		row_no    = find(row_no);  
+		column_no = find(column_no);  
 		
-		if(visited[row_no] == 0 || visited[column_no] ==0){
-			printf("\nEdge %d is (%d -> %d) with cost : %d ",count++,vertex1,vertex2,min_cost);
+		if(uni(row_no,column_no)){  
+			printf("\nEdge %d is (%d -> %d) with cost : %d ",count++,edge1,edge2,min_cost);
 			sum_cost = sum_cost + min_cost;
-			visited[column_no]=1;
 		}
-		cost_matrix[vertex1][vertex2] = cost_matrix[vertex2][vertex1] = 999;
-	}
-	printf("\n Minimum cost=%d",sum_cost); 
+		cost_matrix[edge1][edge2] = cost_matrix[edge2][edge1] = 999;
+	}    
+	printf("\n Minimum cost=%d",sum_cost);  
 	return 0;
-}
+} 
+					  				  
+
+
