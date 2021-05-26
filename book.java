38175fb01da6f66381b3bea44528fd247184e758
techworldthink <jobin25051999@gmail.com>
2021-05-26 12:46:20 +0530

    book object array

--- book.java
+++ book.java
@@ -1,23 +1,52 @@
- class Book {
+class Book {
     int ISBN;
     String title;
     String author;
     int price;
     String Publisher;
+    
+    Book(){
 
-    Book(int isbn,String title,String author,int price,String publisher){
+    }
+
+    Book(int isbn, String title, String author, int price, String publisher) {
         this.ISBN = isbn;
         this.title = title;
         this.author = author;
         this.price = price;
         this.Publisher = publisher;
     }
-    public void display(){
-        System.out.println(this.ISBN);
-        System.out.println(this.title);
-        System.out.println(this.author);
-        System.out.println(this.price);
-        System.out.println(this.Publisher);
+
+    public void display() {
+        System.out.print(this.ISBN + "\t");
+        System.out.print(this.title + "\t");
+        System.out.print(this.author + "\t");
+        System.out.print(this.price + "\t");
+        System.out.print(this.Publisher + "\t");
+        System.out.println();
     }
+
     public static void main(String []args){
+        Book books[] = new Book[5];
+        books[0] =new Book(1,"A","S",11,"AD");
+        books[1] =new Book(2,"B","D",12,"AA");
+        books[2] =new Book(3,"AC","F",13,"AA");
+        books[3] =new Book(4,"E","A",14,"AA");
+        books[4] =new Book(5,"DA","G",15,"AA");
+
+        int size = 5;  
+        for(int i = 0; i<size-1; i++) { 
+            for (int j = i+1; j<size; j++) { 
+                if(books[i].title.compareTo(books[j].title)>0) {   
+                    Book temp = books[i];  
+                    books[i] = books[j];  
+                    books[j] = temp;  
+                }  
+            }  
+        }  
+
+        for(int i=0;i<5;i++){
+            books[i].display();
+        }
+    }
 }
