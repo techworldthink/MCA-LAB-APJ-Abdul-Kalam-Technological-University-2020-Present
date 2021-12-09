4f987dbe777b70bbc9985f54024556c80d5419b9
techworldthink <jobin25051999@gmail.com>
2021-12-09 22:06:25 +0530

    S3 DS 3.8

---
+++ S3-LAB/Data Science Lab/EXERCISE 3/8_Matplotlib.py
@@ -0,0 +1,21 @@
+"""
+8. Write a Python programming to create a pie chart
+   of the popularity of programming Languages.
+   
+Programming languages: Java Python PHP JavaScript C# C++
+Popularity           : 22.2 17.6   8.8 8          7.7 6.7
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([22.2,17.6,8.8,8,7.7,6.7])
+mylabels = ["Java", "Python", "PHP", "JavaScript", "C#", "C++"]
+
+plt.pie(y, labels = mylabels)
+plt.show() 
+
+
+
+
