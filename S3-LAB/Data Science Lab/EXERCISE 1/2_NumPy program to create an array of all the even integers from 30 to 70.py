cba9a1d948b769cbd234d3c6318f2d8c2a0e0ed2
techworldthink <jobin25051999@gmail.com>
2021-12-11 20:04:53 +0530

    DS 1 error fix

--- S3-LAB/Data Science Lab/EXERCISE 1/2_NumPy program to create an array of all the even integers from 30 to 70.py
+++ S3-LAB/Data Science Lab/EXERCISE 1/2_NumPy program to create an array of all the even integers from 30 to 70.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-x = np.arange(start=30, stop=71, step=1)
+x = np.arange(start=30, stop=71, step=2)
 print(x)
--- S3-LAB/Data Science Lab/EXERCISE 1/9_ NumPy program to create a 4x4 array with random values, now create a new array.py
+++ S3-LAB/Data Science Lab/EXERCISE 1/9_ NumPy program to create a 4x4 array with random values, now create a new array.py
@@ -7,12 +7,16 @@ print(nums)
 print("\nNew array after swapping first and last rows of the said array:")
 #new_nums = nums[3:3:-1]
 
+nums = nums[[-1,1,2,0]]
+print(nums)
+
+"""
 num0 = list(nums[0])
 num3 = list(nums[3])
 nums[0] = num3
 nums[3] = num0
 print(nums)
-
+"""
 
 #####################################################################
 
