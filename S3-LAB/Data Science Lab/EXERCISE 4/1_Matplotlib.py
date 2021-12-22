a3316fc274237409bb5065bb79c621d3af8d04ed
techworldthink <jobin25051999@gmail.com>
2021-12-22 20:10:28 +0530

    Exp 4 Q

---
+++ S3-LAB/Data Science Lab/EXERCISE 4/1_Matplotlib.py
@@ -0,0 +1,14 @@
+"""
+1. Draw a line in a diagram from position (1, 3) to (2, 10) then to (6, 12) and finally to position 
+(18, 20). (Mark each point with a beautiful green colour and set line colour to red and line style 
+dotted)
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 18])
+ypoints = np.array([3, 10, 12, 20])
+
+plt.plot(xpoints, ypoints,marker = 'o',color="red",mec = 'g', mfc = 'g',linestyle = 'dotted')
+plt.show()
