36d332cb895bace057626f23d406c5189e803318
techworldthink <jobin25051999@gmail.com>
2021-12-22 20:48:46 +0530

    Exp 4.4

---
+++ S3-LAB/Data Science Lab/EXERCISE 4/4_Pandas.py
@@ -0,0 +1,13 @@
+"""
+4. Given a 2D List, convert it into corresponding dataframe and display it.
+
+"""
+
+
+import pandas as pd
+
+details = [[1,2],[3,4]]
+  
+df = pd.DataFrame(details)
+  
+print(df)
