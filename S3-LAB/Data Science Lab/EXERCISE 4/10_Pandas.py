0853573f00943129361469148e6a6051040649fd
techworldthink <jobin25051999@gmail.com>
2021-12-22 22:20:44 +0530

    Exp 4.10

---
+++ S3-LAB/Data Science Lab/EXERCISE 4/10_Pandas.py
@@ -0,0 +1,14 @@
+"""
+10. Given a dataframe with NaN Values, fill the NaN values with 0.
+
+"""
+
+
+import pandas as pd
+import numpy as np
+
+nums = {'Set_of_Numbers': [2, 3, 5, 7, 11, 13,np.nan, 19, 23, np.nan]}
+df = pd.DataFrame(nums, columns =['Set_of_Numbers'])
+df['Set_of_Numbers'] = df['Set_of_Numbers'].fillna(0)
+print(df)
+
