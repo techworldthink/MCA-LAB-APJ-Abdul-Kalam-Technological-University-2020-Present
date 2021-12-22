0142caec9d989b0326c8a016302a169ced76da39
techworldthink <jobin25051999@gmail.com>
2021-12-22 21:12:13 +0530

    Exp 4.7

---
+++ S3-LAB/Data Science Lab/EXERCISE 4/7_Pandas.py
@@ -0,0 +1,22 @@
+"""
+7. Given a dataframe with custom indexing,
+   convert and it to default indexing and 
+   display it.
+
+"""
+
+
+import pandas as pd
+
+data = {'Name': ['e','a','a','b','c','d'],
+                   'Age': [1,2,1,3,3,4],
+                   'Rank': [0,1,2,3,4,5]}
+
+index = {'a1', 'b1', 'c1', 'd1', 'e1','f1'}
+
+df = pd.DataFrame(data,index)
+#df.reset_index(inplace = True)
+print(df.to_string())
+df.reset_index(inplace = True, drop = True)
+print(df.to_string())
+
