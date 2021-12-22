1515b394a963bb1615e8f9f02df7967f823c1d60
techworldthink <jobin25051999@gmail.com>
2021-12-22 22:55:41 +0530

    Exp 3.6 value fixed

--- S3-LAB/Data Science Lab/EXERCISE 3/6_Matplotlib_2.py
+++ S3-LAB/Data Science Lab/EXERCISE 3/6_Matplotlib_2.py
@@ -1,11 +1,12 @@
 """
 6. Consider the following data.
 Programming languages: Java  Python  PHP  JavaScript  C#  C++
-Popularity             22.2  17.6    8.8  8           77  6.7
+Popularity             22.2  17.6    8.8  8           7.7  6.7
 
 (ii) Write a Python programming to display a horizontal bar chart
 of the popularity of programming 
-Languages(Give Red colour to the bar chart).
+Languages(Give Red colour to the bar chart).
+
 
 """
 
@@ -14,7 +15,7 @@ import matplotlib.pyplot as plt
 
 
 # creating the dataset
-data = {'Java':22.2, 'Python':17.6, 'PHP':8.8,'JavaScript':8,'C#':77,'C++':6.7}
+data = {'Java':22.2, 'Python':17.6, 'PHP':8.8,'JavaScript':8,'C#':7.7,'C++':6.7}
 
 courses = list(data.keys())
 values = list(data.values())
--- S3-LAB/Data Science Lab/EXERCISE 3/6_Matplotlib_3.py
+++ S3-LAB/Data Science Lab/EXERCISE 3/6_Matplotlib_3.py
@@ -1,11 +1,12 @@
 """
 6. Consider the following data.
 Programming languages: Java  Python  PHP  JavaScript  C#  C++
-Popularity             22.2  17.6    8.8  8           77  6.7
+Popularity             22.2  17.6    8.8  8           7.7  6.7
 
 (iii) Write a Python programming to display a bar chart of the
       popularity of programming Languages. 
-      Use different color for each bar.
+      Use different color for each bar.
+
 
 
 """
@@ -15,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 # creating the dataset
-data = {'Java':22.2, 'Python':17.6, 'PHP':8.8,'JavaScript':8,'C#':77,'C++':6.7}
+data = {'Java':22.2, 'Python':17.6, 'PHP':8.8,'JavaScript':8,'C#':7.7,'C++':6.7}
 color=("red","yellow","maroon","green","black","cyan")
 courses = list(data.keys())
 values = list(data.values())
