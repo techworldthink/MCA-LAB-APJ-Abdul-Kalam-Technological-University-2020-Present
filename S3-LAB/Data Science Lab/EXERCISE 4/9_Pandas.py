4b8156eef23af5c9c0aa01ef54ae4d57a8291bf9
techworldthink <jobin25051999@gmail.com>
2021-12-22 22:14:30 +0530

    Exp 4.9

---
+++ S3-LAB/Data Science Lab/EXERCISE 4/9_Pandas.py
@@ -0,0 +1,24 @@
+"""
+9. Given is a dataframe showing name, occupation,
+   salary of people. Find the average 
+   salary per occupation.
+
+"""
+
+
+import pandas as pd
+
+details = {
+    'Name' : ['a','b','c','d','e'],
+    'Occupation' : ['A1','A1','A1','B1','B1'],
+    'Salary' : [20,30,40,27,23],
+}
+  
+
+df = pd.DataFrame(details)
+print(df)
+occ_average_age = df.groupby('Occupation')['Salary'].mean()
+print("Average salary per occupation : ")
+print(occ_average_age)
+
+
