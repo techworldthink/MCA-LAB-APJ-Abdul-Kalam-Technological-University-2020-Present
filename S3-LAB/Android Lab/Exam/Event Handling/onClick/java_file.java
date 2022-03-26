24fbdadf97460f7382aa4f3706ce1f826929b88f
techworldthink <jobin25051999@gmail.com>
2022-03-26 20:36:41 +0530

    MAD exam - 2

---
+++ S3-LAB/Android Lab/Exam/Event Handling/onClick/java_file.java
@@ -0,0 +1,22 @@
+package com.example.pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Handle btn click event
+    public void btnClick(View view){
+        Toast.makeText(MainActivity.this,"Button Clicked",Toast.LENGTH_LONG).show();
+    }
+
+}
