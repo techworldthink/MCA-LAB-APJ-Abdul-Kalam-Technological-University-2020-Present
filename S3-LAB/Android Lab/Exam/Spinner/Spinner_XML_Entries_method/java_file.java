07d4a26a96cb0eaca5eb5679932515d1b4ac419b
techworldthink <jobin25051999@gmail.com>
2022-03-29 17:27:54 +0530

    MAD exam - 12

---
+++ S3-LAB/Android Lab/Exam/Spinner/Spinner_XML_Entries_method/java_file.java
@@ -0,0 +1,34 @@
+package com.example.pro;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, "Selected Item "+ adapterView.getItemAtPosition(i),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
